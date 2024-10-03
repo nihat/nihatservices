548c91e48d28ac1224a216ea2d3da83564d095f0
nkiziltoprak <topraknihat@gmail.com>
2024-10-03 10:48:56 +0300

    initial commit db

--- customer/src/main/java/com/trius/customer/Customer.java
+++ customer/src/main/java/com/trius/customer/Customer.java
@@ -1,13 +1,22 @@
 package com.trius.customer;
 
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
 
+    @Id
+    @SequenceGenerator(name = "customer_id_seq",sequenceName = "customer_id_seq")
+    @GeneratedValue(generator ="customer_id_seq",strategy = GenerationType.SEQUENCE)
     private Integer id;
     private String firstName;
     private String lastName;
--- customer/src/main/java/com/trius/customer/CustomerService.java
+++ customer/src/main/java/com/trius/customer/CustomerService.java
@@ -1,11 +1,13 @@
 package com.trius.customer;
 
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder().
                 firstName(request.firstName()).
                 lastName(request.lastName()).
                 email(request.email()).build();
+
+        customerRepository.save(customer);
     }
 }
